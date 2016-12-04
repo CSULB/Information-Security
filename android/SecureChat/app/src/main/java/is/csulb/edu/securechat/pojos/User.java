ac7b4666532a7b87e1f70331bf27fb3dc19dc8a4
DIVYA <divyareddy2910@gmail.com>
2016-12-03 17:41:38 -0800

    Added sodium. Testing.

--- android/SecureChat/app/src/main/java/is/csulb/edu/securechat/pojos/User.java
+++
@@ -1,8 +0,0 @@
-package is.csulb.edu.securechat.pojos;
-
-public class User {
-
-    public long id;
-    public String password, confirm_password, phone, first_name, last_name, challenge_response;
-
-}
--- android/SecureChat/app/src/main/java/is/csulb/edu/securechat/utils/StaticMembers.java
+++
@@ -1,5 +0,0 @@
-package is.csulb.edu.securechat.utils;
-
-public class StaticMembers {
-    public static final String LOGGED_IN = "LOGGED_IN";
-}
